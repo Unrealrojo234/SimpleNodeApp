c3ebcb733615d0c809386a21ff9dd38a0e7861e5
Unrealrojo234 <otienoryan812@gmail.com>
2024-08-15 21:18:02 +0300

    Added Delete API

--- index.js
+++ index.js
@@ -1,66 +1,78 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const cors = require('cors');
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
 const app = express();
 app.use(cors());
 
 //Middleware to parse JSON with an increased limit (e.g., 20MB)
-app.use(bodyParser.json({limit:'5mb'}));
-
+app.use(bodyParser.json({ limit: "5mb" }));
 
 //Importing Models
-const Info = require('./models/profile');
+const Info = require("./models/profile");
 
 //Getting Database connection string
-const connectionString =  process.env.DATABASE_CONNECTION_STRING;
+const connectionString = process.env.DATABASE_CONNECTION_STRING;
 
 //Express Middlewares
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req,res)=>{
-    res.send('<h1>Simple Web App Test</h1>')
+app.get("/", (req, res) => {
+  res.send("<h1>Simple Web App Test</h1>");
 });
-
-
 
 //Posting Data to the database
-app.post('/info', async (req,res)=>{
-    try{
-        const info = await Info.create(req.body);
-        res.status(200).json(info);
-        res.send(console.log(info))
-
-    }catch(error){
-       res.status(500).json({
-        message:error.message
-       })
-    }
-})
-
-//Getting data from the Database
-app.get('/info', async(req,res)=>{
-    try{
-        const info = await Info.find({})
-        res.status(200).json(info)
-
-    }catch(error){
-        res.status(500).json({
-            message: error.message
-        })
-    }
-})
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000')
+app.post("/info", async (req, res) => {
+  try {
+    const info = await Info.create(req.body);
+    res.status(200).json(info);
+    res.send(console.log(info));
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 });
 
-mongoose.connect(connectionString)
-    .then(()=>{
-        console.log("Successfully connected to the database");  
-    })
-    .catch(()=>{
-        console.log('Failed to connect to the database');
-    })
+//Getting All data from the Database
+app.get("/info", async (req, res) => {
+  try {
+    const info = await Info.find({});
+    res.status(200).json(info);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+//Deleting Data from DataBase
+app.delete("/info/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const info = await Info.findByIdAndDelete(id);
+
+    if (!info) {
+      return res.status(404).json({ message: "Character not found" });
+    }
+
+    res.status(200).json({ message: "Character Deleted Successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch(() => {
+    console.log("Failed to connect to the database");
+  });
